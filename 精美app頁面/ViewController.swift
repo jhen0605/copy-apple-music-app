86fefa7ee9b6987cc42de2888d9355a5f7b8c66c
jhen0605 <amy95173@gmail.com>
2021-03-11 00:24:32 +0800

    Initial Commit

---
+++ 精美app頁面/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  精美app頁面
+//
+//  Created by 簡吟真 on 2021/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
